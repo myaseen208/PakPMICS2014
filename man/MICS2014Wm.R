426967585e6519ec1bcbb2d44561e1828d0f6883
MYaseen208 <myaseen208@gmail.com>
2017-09-06 10:56:39 +0500

    Documentation updated

---
+++ man/MICS2014Wm.R
@@ -0,0 +1,334 @@
+#' @title Data of eligible women age 15-49 years, MICS Punjab, 2014 
+#' @name   MICS2014Wm
+#' @docType data
+#' @keywords datasets
+#' @usage data(MICS2014Wm)
+#' @description \code{MICS2014Wm} Data of eligible women age 15-49 years, MICS Punjab, 2014.
+#' @format A data frame with 61286 rows and 308 variables:
+#' \describe{
+#'   \item{\code{HH1}}{Cluster number}
+#'   \item{\code{HH2}}{Household number}
+#'   \item{\code{LN}}{Line number}
+#'   \item{\code{WM1}}{Cluster number}
+#'   \item{\code{WM2}}{Household number}
+#'   \item{\code{WM4}}{Woman's line number}
+#'   \item{\code{WM5}}{Interviewer number}
+#'   \item{\code{WM6D}}{Day of interview}
+#'   \item{\code{WM6M}}{Month of interview}
+#'   \item{\code{WM6Y}}{Year of interview}
+#'   \item{\code{WM7}}{Result of woman's interview}
+#'   \item{\code{WM8}}{Field editor}
+#'   \item{\code{WM9}}{Data entry clerk}
+#'   \item{\code{WM10H}}{Start of interview - Hour}
+#'   \item{\code{WM10M}}{Start of interview - Minutes}
+#'   \item{\code{WM11H}}{End of interview - Hour}
+#'   \item{\code{WM11M}}{End of interview - Minutes}
+#'   \item{\code{WB1M}}{Month of birth of woman}
+#'   \item{\code{WB1Y}}{Year of birth of woman}
+#'   \item{\code{WB2}}{Age of woman}
+#'   \item{\code{WB3}}{Ever attended school}
+#'   \item{\code{WB4}}{Highest level of school attended}
+#'   \item{\code{WB5}}{Highest grade/class completed at that level}
+#'   \item{\code{WB7}}{Can read part of the sentence}
+#'   \item{\code{WDOI}}{Date of interview women (CMC)}
+#'   \item{\code{WB1C}}{Date of birth of woman (CMC)}
+#'   \item{\code{WB1F}}{Date flag for WB1}
+#'   \item{\code{WB2C}}{Age of respondent (calculated)}
+#'   \item{\code{WAGE}}{Age}
+#'   \item{\code{MT2}}{Frequency of reading newspaper or magazine}
+#'   \item{\code{MT3}}{Frequency of listening to the radio}
+#'   \item{\code{MT4}}{Frequency of watching TV}
+#'   \item{\code{MT6}}{Ever used a computer}
+#'   \item{\code{MT7}}{Computer usage in the last 12 months}
+#'   \item{\code{MT8}}{Frequency of computer usage in the last month}
+#'   \item{\code{MT9}}{Ever used internet}
+#'   \item{\code{MT10}}{Internet usage in the last 12 months}
+#'   \item{\code{MT11}}{Frequency of Internet usage in the past month}
+#'   \item{\code{MA1}}{Currently married}
+#'   \item{\code{MA2}}{Age of husband}
+#'   \item{\code{MA3}}{Husband has other wives}
+#'   \item{\code{MA4}}{Number of other wives}
+#'   \item{\code{MA5}}{Ever married}
+#'   \item{\code{MA6}}{Marital status}
+#'   \item{\code{MA8M}}{Month of first marriage}
+#'   \item{\code{MA8Y}}{Year of first marriage}
+#'   \item{\code{MA9}}{Age at first marriage}
+#'   \item{\code{MA8C}}{Date of first union (CMC)}
+#'   \item{\code{MA8F}}{Date flag for MA8C}
+#'   \item{\code{MA9C}}{Age at first union (calculated)}
+#'   \item{\code{CM1}}{Ever given birth}
+#'   \item{\code{CM2M}}{Month of first birth}
+#'   \item{\code{CM2Y}}{Year of first birth}
+#'   \item{\code{CM3}}{Years since first birth}
+#'   \item{\code{CM4}}{Any sons or daughters living with you}
+#'   \item{\code{CM5A}}{Sons living with you}
+#'   \item{\code{CM5B}}{Daughters living with you}
+#'   \item{\code{CM6}}{Any sons or daughters not living with you}
+#'   \item{\code{CM7A}}{Sons living elsewhere}
+#'   \item{\code{CM7B}}{Daughters living elsewhere}
+#'   \item{\code{CM8}}{Ever had child who later died}
+#'   \item{\code{CM9A}}{Boys dead}
+#'   \item{\code{CM9B}}{Girls dead}
+#'   \item{\code{CM10}}{Children ever born}
+#'   \item{\code{CTOT}}{Check total number of children ever born}
+#'   \item{\code{CM12M}}{Month of last birth}
+#'   \item{\code{CM12Y}}{Year of last birth}
+#'   \item{\code{CM13}}{Last birth in last two years}
+#'   \item{\code{DB1}}{Wanted last child then}
+#'   \item{\code{DB2}}{Wanted child later or did not want more children}
+#'   \item{\code{DB3U}}{Desired waiting time (units)}
+#'   \item{\code{DB3N}}{Desired waiting time (number)}
+#'   \item{\code{MN1}}{Received antenatal care}
+#'   \item{\code{MN2A}}{Antenatal care: Doctor}
+#'   \item{\code{MN2B}}{Antenatal care: Nurse / Midwife}
+#'   \item{\code{MN2D}}{Antenatal care: Lady Health Visitor (LHV)}
+#'   \item{\code{MN2E}}{Antenatal care: Lady Health Worker (LHW)}
+#'   \item{\code{MN2F}}{Antenatal care: Traditional birth attendant (TBA)}
+#'   \item{\code{MN2H}}{Antenatal care: Relatives/friends}
+#'   \item{\code{MN2X}}{Antenatal care: Other}
+#'   \item{\code{MN2AU}}{Weeks or months pregnant at first ANC - unit}
+#'   \item{\code{MN2AN}}{Weeks or months pregnant at first ANC - number}
+#'   \item{\code{MN3}}{Times received antenatal care}
+#'   \item{\code{MN4A}}{Blood pressure}
+#'   \item{\code{MN4B}}{Urine sample}
+#'   \item{\code{MN4C}}{Blood sample}
+#'   \item{\code{MN4D}}{Weighted}
+#'   \item{\code{MN5}}{Has own immunization card}
+#'   \item{\code{MN6}}{Any tetanus toxoid injection during last pregnancy}
+#'   \item{\code{MN7}}{Doses of tetanus toxoid during last pregnancy}
+#'   \item{\code{MN9}}{Any tetanus toxoid injection before last pregnancy}
+#'   \item{\code{MN10}}{Doses of tetanus toxoid before last pregnancy}
+#'   \item{\code{MN11}}{Years ago last tetanus toxoid received}
+#'   \item{\code{MN13}}{Took medicine in order to prevent malaria}
+#'   \item{\code{MN14A}}{Medicines taken to prevent malaria: SP / Fansidar}
+#'   \item{\code{MN14B}}{Medicines taken to prevent malaria: Chloroquine}
+#'   \item{\code{MN14X}}{Medicines taken to prevent malaria: Other}
+#'   \item{\code{MN14Z}}{Medicines taken to prevent malaria: DK}
+#'   \item{\code{MN16}}{Times took SP / Fansidar}
+#'   \item{\code{MN17A}}{Assistance at delivery: Doctor}
+#'   \item{\code{MN17B}}{Assistance at delivery: Nurse / Midwife}
+#'   \item{\code{MN17D}}{Assistance at delivery: Lady Health Visitor (LHV)}
+#'   \item{\code{MN17F}}{Assistance at delivery: Traditional birth attendant}
+#'   \item{\code{MN17H}}{Assistance at delivery: Relative / Friend}
+#'   \item{\code{MN17X}}{Assistance at delivery: Other}
+#'   \item{\code{MN17Y}}{Assistance at delivery: No one}
+#'   \item{\code{MN18}}{Place of delivery}
+#'   \item{\code{MN19}}{Delivery by caesarean section}
+#'   \item{\code{MN19A}}{Decision for caesarean made before the onset of labour or after}
+#'   \item{\code{MN20}}{Size of child at birth}
+#'   \item{\code{MN21}}{Child weighed at birth}
+#'   \item{\code{MN22A}}{Weight from card or recall}
+#'   \item{\code{MN22}}{Weight at birth (Kilograms)}
+#'   \item{\code{MN23}}{Menstrual period returned since the birth of child}
+#'   \item{\code{MN24}}{Ever breastfeed}
+#'   \item{\code{MN25U}}{Time baby put to breast (unit)}
+#'   \item{\code{MN25N}}{Time baby put to breast (number)}
+#'   \item{\code{MN26}}{Within first 3 days after delivery, child given anything to drink other than breast milk}
+#'   \item{\code{MN27A}}{Child given to drink - Milk (other than breast milk)}
+#'   \item{\code{MN27B}}{Child given to drink - Plain water}
+#'   \item{\code{MN27C}}{Child given to drink - Sugar or glucose water}
+#'   \item{\code{MN27D}}{Child given to drink - Gripe water/Rose water etc}
+#'   \item{\code{MN27E}}{Child given to drink - Sugar - salt - water solution}
+#'   \item{\code{MN27F}}{Child given to drink - Fruit juice}
+#'   \item{\code{MN27G}}{Child given to drink - Infant formula}
+#'   \item{\code{MN27H}}{Child given to drink - Tea / Infusions}
+#'   \item{\code{MN27I}}{Child given to drink - Honey}
+#'   \item{\code{MN27J}}{Child given to drink - Rose water}
+#'   \item{\code{MN27X}}{Child given to drink - Other}
+#'   \item{\code{MN28}}{HH visited by lady health worker (LHW) during past month}
+#'   \item{\code{MN29A}}{LHW provided - ORT, vitamins, medicines}
+#'   \item{\code{MN29B}}{LHW provided - weighed child}
+#'   \item{\code{MN29C}}{LHW provided - education / advice}
+#'   \item{\code{MN29X}}{LHW provided - Other (specify)}
+#'   \item{\code{MN29Z}}{LHW provided - DK}
+#'   \item{\code{PN2U}}{Duration of staying in the health facility (unit)}
+#'   \item{\code{PN2N}}{Duration of staying in the health facility (number)}
+#'   \item{\code{PN3}}{Baby's health checked before leaving health facility}
+#'   \item{\code{PN4}}{Mother's health checked before leaving health facility}
+#'   \item{\code{PN5}}{Baby's health checked after leaving health facility}
+#'   \item{\code{PN7}}{Baby checked after the delivery was over}
+#'   \item{\code{PN8}}{Mother checked after the delivery was over}
+#'   \item{\code{PN9}}{Additional baby check after the delivery}
+#'   \item{\code{PN10}}{Baby checked after the delivery}
+#'   \item{\code{PN11}}{Number of times baby was checked}
+#'   \item{\code{PN12U}}{How long after delivery did the first check of baby happen (unit)}
+#'   \item{\code{PN12N}}{How long after delivery did the first check of baby happen (number)}
+#'   \item{\code{PN13A}}{Person checking on baby's health: Doctor}
+#'   \item{\code{PN13B}}{Person checking on baby's health: Nurse / Midwife}
+#'   \item{\code{PN13D}}{Person checking on baby's health: Lady Health Visitor (LHV)}
+#'   \item{\code{PN13E}}{Person checking on baby's health: Lady Health Worker (LHW)}
+#'   \item{\code{PN13F}}{Person checking on baby's health: Traditional birth attendant}
+#'   \item{\code{PN13H}}{Person checking on baby's health: Relative / Friend}
+#'   \item{\code{PN13X}}{Person checking on baby's health: Other}
+#'   \item{\code{PN14}}{Place where baby's health was checked}
+#'   \item{\code{PN16}}{Mother's health checked after leaving health facility}
+#'   \item{\code{PN18}}{Mother's health checked after the delivery was over}
+#'   \item{\code{PN19}}{Mother's health checked after the birth of the baby}
+#'   \item{\code{PN20}}{Number of times mother's health was checked after the birth}
+#'   \item{\code{PN21U}}{How long after delivery did the first check of mother happen (unit)}
+#'   \item{\code{PN21N}}{How long after delivery did the first check of mother happen (number)}
+#'   \item{\code{PN22A}}{Person checking on mother's health: Doctor}
+#'   \item{\code{PN22B}}{Person checking on mother's health: Nurse / Midwife}
+#'   \item{\code{PN22D}}{Person checking on mother's health: LHV}
+#'   \item{\code{PN22E}}{Person checking on mother's health: Lady Health Worker (LHW)}
+#'   \item{\code{PN22F}}{Person checking on mother's health: Traditional birth attendant}
+#'   \item{\code{PN22H}}{Person checking on mother's health: Relative / Friend}
+#'   \item{\code{PN22X}}{Person checking on mother's health: Other}
+#'   \item{\code{PN23}}{Place where mother's health was checked}
+#'   \item{\code{IS2A}}{Symptoms: Child not able to drink or breastfeed}
+#'   \item{\code{IS2B}}{Symptoms: Child becomes sicker}
+#'   \item{\code{IS2C}}{Symptoms: Child develops a fever}
+#'   \item{\code{IS2D}}{Symptoms: Child has fast breathing}
+#'   \item{\code{IS2E}}{Symptoms: Child has difficulty breathing}
+#'   \item{\code{IS2F}}{Symptoms: Child has blood in stools}
+#'   \item{\code{IS2G}}{Symptoms: Child is drinking poorly}
+#'   \item{\code{IS2H}}{Symptoms: Child suffered from lose motion}
+#'   \item{\code{IS2X}}{Symptoms: Other}
+#'   \item{\code{IS2Y}}{Symptoms: Other}
+#'   \item{\code{IS2Z}}{Symptoms: Other}
+#'   \item{\code{CP1}}{Currently pregnant}
+#'   \item{\code{CP2}}{Currently using a method to avoid pregnancy}
+#'   \item{\code{CP2A}}{Ever used a method to avoid pregnancy}
+#'   \item{\code{CP3A}}{Current method: Female sterilization}
+#'   \item{\code{CP3B}}{Current method: Male sterilization}
+#'   \item{\code{CP3C}}{Current method: IUD}
+#'   \item{\code{CP3D}}{Current method: Injectables}
+#'   \item{\code{CP3E}}{Current method: Implants}
+#'   \item{\code{CP3F}}{Current method: Pill}
+#'   \item{\code{CP3G}}{Current method: Male condom}
+#'   \item{\code{CP3H}}{Current method: Female condom}
+#'   \item{\code{CP3I}}{Current method: Diaphragm}
+#'   \item{\code{CP3L}}{Current method: Periodic abstinence / Rhythm}
+#'   \item{\code{CP3M}}{Current method: Withdrawal}
+#'   \item{\code{CP3X}}{Current method: Other}
+#'   \item{\code{UN2}}{Wanted to get pregnant at the time}
+#'   \item{\code{UN3}}{Wanted baby later or did not want more children}
+#'   \item{\code{UN4}}{Would like to have another child (currently pregnant)}
+#'   \item{\code{UN6}}{Would like to have another child (not currently pregnant)}
+#'   \item{\code{UN7U}}{Desired waiting time (unit)}
+#'   \item{\code{UN7N}}{Desired waiting time (number)}
+#'   \item{\code{UN10}}{Able to get pregnant}
+#'   \item{\code{UN11A}}{Reason: Infrequent sex / No sex}
+#'   \item{\code{UN11B}}{Reason: Menopausal}
+#'   \item{\code{UN11C}}{Reason: Never menstruated}
+#'   \item{\code{UN11D}}{Reason: Hysterectomy}
+#'   \item{\code{UN11E}}{Reason: Trying to get pregnant for 2 years without result}
+#'   \item{\code{UN11F}}{Reason: Postpartum amenorrheic}
+#'   \item{\code{UN11G}}{Reason: Breastfeeding}
+#'   \item{\code{UN11H}}{Reason: Too old}
+#'   \item{\code{UN11I}}{Reason: Fatalistic}
+#'   \item{\code{UN11X}}{Reason: Other}
+#'   \item{\code{UN11Z}}{Reason: Don't know}
+#'   \item{\code{UN13U}}{Start of last menstrual period (unit)}
+#'   \item{\code{UN13N}}{Start of last menstrual period (number)}
+#'   \item{\code{DV1A}}{If she goes out with out telling husband: wife beating justified}
+#'   \item{\code{DV1B}}{If she neglects the children: wife beating justified}
+#'   \item{\code{DV1C}}{If she argues with husband: wife beating justified}
+#'   \item{\code{DV1D}}{If she refuses sex with husband: wife beating justified}
+#'   \item{\code{DV1E}}{If she burns the food: wife beating justified}
+#'   \item{\code{HA1}}{Ever heard of AIDS}
+#'   \item{\code{HA2}}{Can avoid AIDS virus by having one uninfected partner}
+#'   \item{\code{HA3}}{Can get AIDS virus through supernatural means}
+#'   \item{\code{HA4}}{Can avoid AIDS virus by using a condom correctly every time}
+#'   \item{\code{HA5}}{Can get AIDS virus from mosquito bites}
+#'   \item{\code{HA6}}{Can get AIDS virus by sharing food with a person who has AIDS}
+#'   \item{\code{HA7}}{Healthy-looking person may have AIDS virus}
+#'   \item{\code{HA8A}}{AIDS virus from mother to child during pregnancy}
+#'   \item{\code{HA8B}}{AIDS virus from mother to child during delivery}
+#'   \item{\code{HA8C}}{AIDS virus from mother to child through breastfeeding}
+#'   \item{\code{HA9}}{Should female teacher with AIDS virus be allowed to teach in school}
+#'   \item{\code{HA10}}{Would buy fresh vegetables from shopkeeper with AIDS virus}
+#'   \item{\code{HA11}}{If HH member became infected with AIDS virus, would want it to remain a secret}
+#'   \item{\code{HA12}}{Willing to care for person with AIDS in household}
+#'   \item{\code{HA15A}}{Given information about AIDS virus during antenatal care visit: AIDS from mother}
+#'   \item{\code{HA15B}}{Given information about AIDS virus during antenatal care visit: Things to do}
+#'   \item{\code{HA15C}}{Given information about AIDS virus during antenatal care visit: Tested for AIDS}
+#'   \item{\code{HA15D}}{Given information about AIDS virus during antenatal care visit: Offered a test}
+#'   \item{\code{HA16}}{Tested for AIDS virus as part of antenatal care}
+#'   \item{\code{HA17}}{Received results from test during antenatal care}
+#'   \item{\code{HA18}}{Received consultation after testing during antenatal care}
+#'   \item{\code{HA20}}{Tested for AIDS virus during delivery}
+#'   \item{\code{HA21}}{Received results from test during delivery}
+#'   \item{\code{HA22}}{Tested for AIDS virus since test during pregnancy}
+#'   \item{\code{HA23}}{Most recent time of testing for AIDS virus}
+#'   \item{\code{HA24}}{Ever been tested for AIDS virus}
+#'   \item{\code{HA25}}{Most recent time of testing for AIDS virus}
+#'   \item{\code{HA26}}{Received results of AIDS virus test}
+#'   \item{\code{HA27}}{Know a place to get AIDS virus test}
+#'   \item{\code{TA1}}{Ever tried cigarette smoking}
+#'   \item{\code{TA2}}{Age when cigarette was smoked for the first time}
+#'   \item{\code{TA3}}{Currently smoking cigarettes}
+#'   \item{\code{TA4}}{Number of cigarettes smoked in the last 24 hours}
+#'   \item{\code{TA5}}{Number of days when cigarettes were smoked in past month}
+#'   \item{\code{TA6}}{Ever tried any smoked tobacco products other than cigarettes}
+#'   \item{\code{TA7}}{Used any smoked tobacco products during the last month}
+#'   \item{\code{TA8A}}{Type of smoked tobacco product: Cigars}
+#'   \item{\code{TA8B}}{Type of smoked tobacco product: Water pipe}
+#'   \item{\code{TA8C}}{Type of smoked tobacco product: Cigarillos}
+#'   \item{\code{TA8D}}{Type of smoked tobacco product: Pipe}
+#'   \item{\code{TA8X}}{Type of smoked tobacco product: Other}
+#'   \item{\code{TA9}}{Number of days when tobacco products where smoked in past month}
+#'   \item{\code{TA10}}{Ever tried any form of smokeless tobacco products}
+#'   \item{\code{TA11}}{Used any smokeless tobacco products during the last month}
+#'   \item{\code{TA12A}}{Type of smokeless tobacco product used: Chewing tobacco}
+#'   \item{\code{TA12D}}{Type of smokeless tobacco product used: Paan with tobacco}
+#'   \item{\code{TA12E}}{Type of smokeless tobacco product used: Gutka}
+#'   \item{\code{TA12F}}{Type of smokeless tobacco product used: Naswar}
+#'   \item{\code{TA12X}}{Type of smokeless tobacco product used: Other}
+#'   \item{\code{TA13}}{Number of days when smokeless tobacco products where used in past month}
+#'   \item{\code{LS2}}{Estimation of overall happiness}
+#'   \item{\code{LS3}}{Satisfaction with family life}
+#'   \item{\code{LS4}}{Satisfaction with friendships}
+#'   \item{\code{LS5}}{School attendance during the current school year}
+#'   \item{\code{LS6}}{Satisfaction with school}
+#'   \item{\code{LS7}}{Satisfaction with current job}
+#'   \item{\code{LS8}}{Satisfaction with health}
+#'   \item{\code{LS9}}{Satisfaction with current residence}
+#'   \item{\code{LS10}}{Satisfaction with treatment by other people}
+#'   \item{\code{LS11}}{Satisfaction with appearance}
+#'   \item{\code{LS12}}{Satisfaction with life overall}
+#'   \item{\code{LS13}}{Satisfaction with current income}
+#'   \item{\code{LS14}}{Life satisfaction in comparison with last year}
+#'   \item{\code{LS15}}{Life satisfaction expectation one year from now}
+#'   \item{\code{HH6}}{Area (Original)}
+#'   \item{\code{HH7}}{District (Original)}
+#'   \item{\code{WDOB}}{Date of birth of woman (CMC)}
+#'   \item{\code{WDOM}}{Date of marriage (CMC)}
+#'   \item{\code{WAGEM}}{Age at first marriage}
+#'   \item{\code{WDOBFC}}{Date of birth of first child (CMC)}
+#'   \item{\code{WDOBLC}}{Date of birth of last child (CMC)}
+#'   \item{\code{MSTATUS}}{Marital status}
+#'   \item{\code{CEB}}{Children ever born}
+#'   \item{\code{CSURV}}{Children surviving}
+#'   \item{\code{CDEAD}}{Children dead}
+#'   \item{\code{welevel}}{Education}
+#'   \item{\code{division}}{Division}
+#'   \item{\code{hh7r}}{District}
+#'   \item{\code{hh6r}}{Area}
+#'   \item{\code{suburban}}{}
+#'   \item{\code{stratum}}{Straum (Original)}
+#'   \item{\code{wmweight}}{Women's sample weight}
+#'   \item{\code{wscore}}{Combined wealth score}
+#'   \item{\code{windex5}}{Wealth index quintile}
+#'   \item{\code{wscoreu}}{Urban wealth score}
+#'   \item{\code{windex5u}}{Urban wealth index quintile}
+#'   \item{\code{wscorer}}{Rural wealth score}
+#'   \item{\code{windex5r}}{Rural wealth index quintile} 
+#'}
+#'   
+#' @author 
+#'  \enumerate{
+#'   \item{Muhammad Yaseen } {(\email{myaseen208@@gmail.com})}
+#'   \item{Muhammad Usman } {(\email{usmann75@hotmail.com})}
+#'   }
+#'   
+#' @seealso 
+#'    \code{\link{MICS2014Ch}}
+#'  , \code{\link{MICS2014HH}}
+#'  , \code{\link{MICS2014HL}}
+#'  
+#' @importFrom data.table data.table
+#' 
+#' @examples
+#' data(MICS2014Wm)
+NULL
